b9604927fee6b744472cdb334aa6544133d75644
adorechic <adorechic@gmail.com>
2015-02-08 22:58:53 +0900

    Show full title and summary

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -8,11 +8,8 @@ class FeedsController < UIViewController
 
 
     category_controller.client.get('/v3/streams/contents', streamId: category["id"]) do |result|
-      titles = result.object["items"].map do |entry|
-        entry["title"]
-      end
-
-      show_entries(titles)
+      feedly_items = result.object["items"]
+      show_entries(feedly_items)
     end
 
     rmq(self.view).apply_style :root_view
@@ -33,8 +30,8 @@ class FeedsController < UIViewController
     self.dismissViewControllerAnimated(true, completion: nil)
   end
 
-  def show_entries(titles)
-    @titles = titles
+  def show_entries(feedly_items)
+    @feedly_items = feedly_items
 
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     @table.dataSource = self
@@ -48,17 +45,21 @@ class FeedsController < UIViewController
   def tableView(tableView, cellForRowAtIndexPath: indexPath)
     @reuseIdentifier ||= "CELL_IDENTIFIER"
     cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) ||
-           TDBadgedCell.alloc.initWithStyle(
-             UITableViewCellStyleDefault, reuseIdentifier: @reuseIdentifier
+           UITableViewCell.alloc.initWithStyle(
+             UITableViewCellStyleSubtitle, reuseIdentifier: @reuseIdentifier
            )
 
-    title = @titles[indexPath.row]
-    cell.textLabel.text = title
+    item = @feedly_items[indexPath.row]
+    cell.textLabel.text = item["title"]
+    cell.textLabel.numberOfLines = 0
+    summary = item["summary"]
+    cell.detailTextLabel.text = summary ? summary["content"] : ""
+    cell.detailTextLabel.numberOfLines = 1
     cell
   end
 
   def tableView(tableView, numberOfRowsInSection: section)
-    @titles.count
+    @feedly_items.count
   end
 
   def tableView(tableView, didEndDisplayingCell: cell, forRowAtIndexPath: indexPath)

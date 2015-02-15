f5ec5651884b5c00a43109ac56bc0912f1de4277
adorechic <adorechic@gmail.com>
2015-02-15 14:03:01 +0900

    Setup CDQ

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,9 @@
 class AppDelegate
+  include CDQ
   attr_reader :window
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    cdq.setup
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
     main_controller = MainController.new

cd170fd1f6781f107d293f188213b5a861b52a48
adorechic <adorechic@gmail.com>
2015-02-16 00:53:50 +0900

    Add pocket token schema

---
+++ schemas/002_create_pocket_token.rb
@@ -0,0 +1,10 @@
+schema "002_create_PocketToken" do
+  entity "FeedlyToken" do
+    string :access_token, optional: false
+    string :refresh_token, optional: false
+  end
+
+  entity "PocketToken" do
+    string :access_token, optional: false
+  end
+end
